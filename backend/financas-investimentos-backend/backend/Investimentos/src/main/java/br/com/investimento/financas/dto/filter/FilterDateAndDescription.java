654bf668f6e6d09d170652f3082f330ffc2a287d
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-12-10 00:44:29 -0300

    breve alterações

--- backend/financas-investimentos-backend/backend/Investimentos/src/main/java/br/com/investimento/financas/dto/filter/FilterDateAndDescription.java
+++ backend/financas-investimentos-backend/backend/Investimentos/src/main/java/br/com/investimento/financas/dto/filter/FilterDateAndDescription.java
@@ -1,12 +1,14 @@
 package br.com.investimento.financas.dto.filter;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.domain.Pageable;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
 @Getter
+@Setter
 public class FilterDateAndDescription {
 
     private int page;
